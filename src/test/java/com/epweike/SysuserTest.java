549ee81e50545bd5beda096a13c79ee09be5b9f5
ping199143 <89768641@qq.com>
2015-03-17 09:13:16 +0800

    单元测试

--- src/test/java/com/epweike/SysuserTest.java
+++ src/test/java/com/epweike/SysuserTest.java
@@ -1,6 +1,5 @@
 package com.epweike;
 
-import org.exolab.castor.types.Date;
 import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -11,33 +10,25 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.epweike.model.Users;
 import com.epweike.service.UsersService;
-import com.epweike.util.MD5Utils;
 
 public class SysuserTest {
 	private static final Logger logger = LoggerFactory.getLogger(SysuserTest.class);
 	
 	@Autowired
-	private UsersService sysuserService;
+	private UsersService usersService;
 
 	@Before
 	public void before() {
 		@SuppressWarnings("resource")
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				new String[] { "classpath:applicationContext.xml" });
-		sysuserService = (UsersService) context.getBean("sysuserService");
+		usersService = (UsersService) context.getBean("usersService");
 	}
 
-//	@Test 
-//	public void countTest() {
-//		Sysuser user = new Sysuser("wxp", "wxp");
-//		Sysuser user2 = (Sysuser) sysuserService.select(user).get(0);
-//		System.out.println(user2.toString() + "---COUNT");
-//	}
-	
 	@Test
 	public void selectOneTest() {
-		Users sysuser = this.sysuserService.selectOne(new Users("ping6"));
-		System.out.println("打印对象：" + sysuser.toString());
+		Users users = this.usersService.selectOne(new Users("ping6dfgdfg"));
+		System.out.println("打印对象：" + users.toString());
 	}
 
 }
